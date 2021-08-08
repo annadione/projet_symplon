cb0a2541aca60e8b9f2ad90f2840e095d67b1deb
AnnaDione <annadione93@gmail.com>
2021-08-08 16:14:51 +0100

    creation api

--- src/Entity/Passager.php
+++ src/Entity/Passager.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PassagerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -9,7 +10,9 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=PassagerRepository::class)
+ * 
  */
+
 class Passager
 {
     /**
